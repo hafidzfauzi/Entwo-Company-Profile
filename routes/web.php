6432f17b84b5931584ced7b0b0ab64a09ad90f21
Hafidz Fauzi <masozi395@gmail.com>
2026-04-14 15:33:14 +0700

    create pages masih kosongan dan setting route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'pages.home')->name('home');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/service', 'pages.service')->name('services');
+Route::view('/contact', 'pages.contact')->name('contact');
+Route::view('/portfolio', 'pages.portfolio')->name('portfolio');
