9fa4b1285f10aef93566743017d00292b75fbd46
Hafidz Fauzi <masozi395@gmail.com>
2026-04-16 09:23:44 +0700

    Penambahan Fitur Swiper dan perbaikan sementara untuk hero section (responsive belum optimal untuk mobile)

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,35 @@
 import './bootstrap';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 
 document.addEventListener('DOMContentLoaded', () => {
+    // Logic Navbar Mobile
     const menuBtn = document.getElementById('menu-btn');
     const mobileMenu = document.getElementById('mobile-menu');
 
     if (menuBtn) {
         menuBtn.addEventListener('click', () => {
             mobileMenu.classList.toggle('hidden');
+        });
+    }
+    // Logic Hero Swiper
+    const swiperElement = document.querySelector('.hero-swiper');
+
+    if (swiperElement) {
+        const heroSwiper = new Swiper('.hero-swiper', {
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            effect: 'fade',
+            fadeEffect: {
+                crossFade: true,
+            },
         });
     }
 });
